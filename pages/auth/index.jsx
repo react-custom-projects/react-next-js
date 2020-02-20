cefce06d6a562d6eea8d11cd9a6b6cbd772216d7
a_morsi <adam.morsi@mindgeek.com>
2020-02-20 11:50:31 +0200

    add user component

--- pages/auth/index.jsx
+++ pages/auth/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+//components
+import User from '../../components/User';
 
 const AuthIndex = () => {
     return (
         <div>
             <p>The auth page</p>
+            <User name="Adam" age="29" />
         </div>
     );
 };
